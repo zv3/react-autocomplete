cde1ffc336d7c85029419f9566192417fc5b0b03
Diego Zacarias <dzv3@me.com>
2022-06-15 23:48:38 -0400

    Documenting the code and improving names

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -13,6 +13,12 @@ interface AutocompleteInput {
   onSelect: (option: AutocompleteOption) => void,
 }
 
+enum KeyboardEventKey {
+  ArrowUp = 'ArrowUp',
+  ArrowDown = 'ArrowDown',
+  Enter = 'Enter',
+}
+
 function Autocomplete({ onChange, options, searchText, placeholder, onSelect }: AutocompleteInput) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [focusedOptionIndex, setFocusedOptionIndex] = useState<number>(-1);
@@ -24,26 +30,15 @@ function Autocomplete({ onChange, options, searchText, placeholder, onSelect }: 
     isFocused: idx === focusedOptionIndex,
   }));
 
-  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    if (searchText !== event.target.value) {
-      setIsExpanded(true);
-    }
-
-    onChange(event);
-  };
-
-  const onSelectHandler = (item: MenuItem) => {
+  const onSelectItemHandler = (item: MenuItem) => {
+    // Close the menu list and reset the focused item when clicking on one.
     setIsExpanded(false);
     setFocusedOptionIndex(-1);
 
     onSelect(item.option);
   };
 
-  const onClickInputHandler: MouseEventHandler = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  const onFocusChangeHandler = (focusedItem?: MenuItem) => {
+  const onFocusChangeItemHandler = (focusedItem?: MenuItem) => {
     if (!focusedItem) {
       setFocusedOptionIndex(-1);
 
@@ -55,18 +50,19 @@ function Autocomplete({ onChange, options, searchText, placeholder, onSelect }: 
     setFocusedOptionIndex(itemIndex);
   };
 
-  const keyHandlers: Record<string, () => void> = {
-    ArrowUp: () => focusedOptionIndex < 0
+  const keyHandlers: Record<KeyboardEventKey, () => void> = {
+    [KeyboardEventKey.ArrowUp]: () => focusedOptionIndex < 0
       ? setFocusedOptionIndex(menuItems.length - 1)
       : setFocusedOptionIndex(focusedOptionIndex - 1),
 
-    ArrowDown: () => {
+    [KeyboardEventKey.ArrowDown]: () => {
       if (!isExpanded) {
-        setIsExpanded(true);
+        setIsExpanded(true); // Display the menu when pressing the arrow-down key.
 
         return;
       }
 
+      // Cycle through the menu list if it's up.
       if (focusedOptionIndex >= menuItems.length) {
         setFocusedOptionIndex(0)
       } else {
@@ -74,20 +70,32 @@ function Autocomplete({ onChange, options, searchText, placeholder, onSelect }: 
       }
     },
 
-    Enter: () => {
+    [KeyboardEventKey.Enter]: () => {
       const focusedItem = menuItems.at(focusedOptionIndex);
-      if (!focusedItem) {
-        return;
+      if (!isExpanded || !focusedItem) {
+        return; // Ignore the key press if the menu isn't up.
       }
 
-      onSelectHandler(focusedItem);
+      onSelectItemHandler(focusedItem);
     }
   };
 
-  const onKeyDownHandler: KeyboardEventHandler = (event) => {
-    const handler = keyHandlers[event.key] || noop;
+  const onKeyDownInputHandler: KeyboardEventHandler = (event) => {
+    const keyHandler = keyHandlers[event.key as KeyboardEventKey] || noop;
 
-    handler();
+    keyHandler();
+  };
+
+  const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (searchText !== event.target.value) {
+      setIsExpanded(true); // Display the menu if the search input has changed.
+    }
+
+    onChange(event);
+  };
+
+  const onClickInputHandler: MouseEventHandler = () => {
+    setIsExpanded(!isExpanded); // Display/hide the menu when clicking on the input box.
   };
 
   return (<div className="autocomplete">
@@ -97,16 +105,16 @@ function Autocomplete({ onChange, options, searchText, placeholder, onSelect }: 
       expanded={isExpanded}
       placeholder={placeholder}
       onClick={onClickInputHandler}
-      onChange={onChangeHandler}
-      onKeyDown={onKeyDownHandler}
+      onChange={onChangeInputHandler}
+      onKeyDown={onKeyDownInputHandler}
     />
 
     {canRenderMenu && <MenuList
       id={id}
       items={menuItems}
       highlightedText={searchText}
-      onSelect={onSelectHandler}
-      onFocusChange={onFocusChangeHandler}
+      onSelect={onSelectItemHandler}
+      onFocusChange={onFocusChangeItemHandler}
     />}
   </div>);
 }
--- src/components/Autocomplete/types.ts
+++ src/components/Autocomplete/types.ts
@@ -3,6 +3,7 @@ export interface AutocompleteOption {
   value: string;
 }
 
+// Type to be used internally by the `Autocomplete` component.
 export interface MenuItem {
   option: AutocompleteOption;
   isFocused: boolean;
